74c3fa9a124571c06b10e078239f6495544606de
Peter-Choah <66401826+Peter-Choah@users.noreply.github.com>
2025-05-28 17:33:37 +0100

    boilerplate

---
+++ Kuwa/src/main.c
@@ -0,0 +1,12 @@
+/* Main Program */
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int a = 5;
+  int b;
+
+  b = 2;
+
+  printf("Filter the Image!\n%d\n", a * 2);
+  printf("Program Ends\n");
+}
