27bb13651ff675eecf5f77a331a46fb1168fec4d
Peter-Choah <66401826+Peter-Choah@users.noreply.github.com>
2025-09-02 15:29:46 +0100

    Image Input working

--- src/app/ImageInput.tsx
+++ src/app/ImageInput.tsx
@@ -8,22 +8,12 @@ interface ImageInputProps {
 }
 
 const ImageInput : FC<ImageInputProps> = ({file, onImageInput}) => {
-    const [file2, setFile2] = useState("");
-    function handleChange(e : React.ChangeEvent<HTMLInputElement>) {
-        console.log(e.target.files);
-        if (e.target.files) {
-        setFile2(URL.createObjectURL(e.target.files[0]));
-        console.log("File Changed");
-        console.log(file);
-        } else {
-        console.log("IFF NOT FIRING!");
-        }
-    }
+
     return (
         <>
             <h2>Add Image:</h2>
-            <input type="file" onChange={handleChange} />
-            {file2 && <img src={file2} className="image-preview" alt="Uploaded preview" />}
+            <input type="file" onChange={onImageInput} />
+            {file && <img src={file} className="image-preview" alt="Uploaded preview" />}
         </>
     )
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import ImageInput from "@/app/ImageInput";
 import { useState } from "react";
 
 export default function Home() {
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState(""); // State for file
 
-  function handleChange(e : React.ChangeEvent<HTMLInputElement>) {
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.files);
+    console.log("Image Inputed");
     if (e.target.files) {
       setFile(URL.createObjectURL(e.target.files[0]));
       console.log("File Changed");
@@ -21,7 +22,6 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
         <ImageInput file={file} onImageInput={handleChange}/>
         <Image
           className="dark:invert"
